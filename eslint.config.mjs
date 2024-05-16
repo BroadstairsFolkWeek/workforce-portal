8e4afed5675c89764d11301cb0fdb839d0319f41
Daniel Watford <daniel@watfordconsulting.com>
2024-05-16 13:31:17 +0000

    Add eslint for the frontend React app

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/jsx-runtime.js";
+
+export default [
+  { ignores: ["api/"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    settings: {
+      react: {
+        version: "17.0",
+      },
+    },
+  },
+];
