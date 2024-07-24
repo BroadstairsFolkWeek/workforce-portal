7392c5929447f2001bbf9da6b6bcc1be01379216
Daniel Watford <daniel@watfordconsulting.com>
2024-07-24 15:49:01 +0000

    Add a TODO component to a Form list item showing the fields that the user needs to fill in

--- src/components/FormSubmissionsListItem.tsx
+++ src/components/FormSubmissionsListItem.tsx
@@ -1,6 +1,7 @@
-import { PropsWithChildren, useCallback } from "react";
-import { FormSubmission } from "../interfaces/form";
+import { PropsWithChildren, useCallback, useMemo } from "react";
 import { DateTime } from "luxon";
+import { Model, Question } from "survey-core";
+import { FormSubmission } from "../interfaces/form";
 import FormSubmissionControls from "./FormSubmissionControls";
 import { useFormSubmissionHandlers } from "../routes/FormSubmissionHandlers";
 
@@ -27,13 +28,58 @@ const ListItemHeader = ({
 
 const ListItemFooter = ({
   children,
+  formSubmission,
 }: PropsWithChildren<{
   formSubmission: FormSubmission;
 }>) => {
+  const errorQuestionNames = useMemo(() => {
+    const m = new Model(formSubmission.formSpec.questions);
+    m.data = formSubmission.answers;
+    const answersAreValid = m.validate();
+
+    if (!answersAreValid) {
+      console.error("Answers are not valid");
+    }
+
+    // Run the validation process, recording any question that fails validation.
+    const validationFailedQuestions: Question[] = [];
+    m.onSettingQuestionErrors.add((sender, options) => {
+      if (options.errors.length > 0) {
+        validationFailedQuestions.push(options.question);
+      }
+    });
+    // Get the validation results from the survey model.
+    m.validate();
+
+    // If there are validation errors, return the list of questions that failed validation.
+    if (validationFailedQuestions.length > 0) {
+      return validationFailedQuestions.map((q) => q.title);
+    } else {
+      return [];
+    }
+  }, [formSubmission]);
+
+  const todoComponent = useMemo(() => {
+    if (errorQuestionNames.length > 0) {
+      return (
+        <div className="text-sm text-white-200">
+          Please EDIT this form and provide the following information:
+          <ul className="ml-4">
+            {errorQuestionNames.map((q) => (
+              <li key={q}>{q}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }, [errorQuestionNames]);
+
   return (
     <div className="bg-bfw-yellow">
       <div className="flex flex-col justify-between p-2 bg-black text-xs text-white">
-        <div>TODO</div>
+        <div>{todoComponent}</div>
       </div>
       <div className="px-2">{children}</div>
     </div>
