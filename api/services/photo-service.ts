343c749245ca4eec4a0462d610f79af1dfb2fba7
Daniel Watford <daniel@watfordconsulting.com>
2024-07-26 08:06:35 +0000

    Remove api code related to getting profile pictures.
    The profile now contains a URL for the client to use to retreive the profile picture.

--- api/services/photo-service.ts
+++
@@ -1,8 +0,0 @@
-export const photoIdFromEncodedPhotoId = (encodedPhotoId: string) => {
-  const splitIds = encodedPhotoId.split(":");
-  if (splitIds.length > 1) {
-    return splitIds[1];
-  } else {
-    return splitIds[0];
-  }
-};
