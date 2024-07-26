01c66a0616bc297be00815d517670bcde6603977
Daniel Watford <daniel@watfordconsulting.com>
2024-07-26 11:29:16 +0000

    Use effect apprach for setting profile photo

--- api/profilePhoto/index.ts
+++ api/profilePhoto/index.ts
@@ -1,101 +1,88 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import { Effect } from "effect";
+import { Data, Effect, Logger, LogLevel, Option } from "effect";
 import { Schema as S } from "@effect/schema";
 
-import { getUserInfo, UserInfo } from "@aaronpowell/static-web-apps-api-auth";
-import { logTrace, logWarn, setLoggerFromContext } from "../utilties/logging";
+import { createLoggerLayer } from "../utilties/logging";
 import {
   getProfileWithFormsByProfile,
   setProfilePicture,
 } from "../services/profile-service";
 import { repositoriesLayerLive } from "../contexts/repositories-live";
 import { SetProfilePhotoResponse } from "../api/photo";
+import { getAuthenticatedUserId } from "../functions/authenticated-user";
 
-const handlePostProfilePhoto = async function (
-  req: HttpRequest,
-  userInfo: UserInfo
-) {
-  const photoContent = req.bufferBody;
-  if (!photoContent) {
-    logWarn("profilePhoto: No file content included in request");
-    return {
-      status: 400,
-      body: "No profile image uploaded",
-    };
-  }
+class NoBodyBuffer extends Data.TaggedClass("NoBodyBuffer") {}
 
-  const contentType = req.headers["content-type"];
-  if (!contentType) {
-    logWarn("profilePhoto: No content type included in request");
-    return {
-      status: 400,
-      body: "No content type included in request",
-    };
-  }
-
-  const setProfilePictureProgram = setProfilePicture(
-    userInfo.userId!,
-    contentType,
-    photoContent
-  )
-    .pipe(
-      Effect.andThen(getProfileWithFormsByProfile(userInfo.userId!)),
-      Effect.andThen((data) => ({ data })),
-      Effect.andThen(S.encode(SetProfilePhotoResponse)),
-      Effect.andThen((body) =>
-        Effect.succeed({
-          status: 200,
-          body,
-        })
-      )
-    )
-    .pipe(
-      Effect.catchTags({
-        ParseError: (e) =>
-          Effect.logError(
-            `ParseError when setting profile photo for user ${userInfo.userId}`,
-            e
-          ).pipe(Effect.andThen(Effect.succeed({ status: 500 }))),
-      })
-    );
-
-  return await Effect.runPromise(
-    setProfilePictureProgram.pipe(Effect.provide(repositoriesLayerLive))
+const getBodyBuffer = (req: HttpRequest) =>
+  Option.fromNullable(req.bufferBody).pipe(
+    Effect.catchTags({
+      NoSuchElementException: () => Effect.fail(new NoBodyBuffer()),
+    })
   );
-};
+
+class NoContentTypeHeader extends Data.TaggedClass("NoContentTypeHeader") {}
+
+const getContentType = (req: HttpRequest) =>
+  Option.fromNullable(req.headers["content-type"]).pipe(
+    Effect.catchTags({
+      NoSuchElementException: () => Effect.fail(new NoContentTypeHeader()),
+    })
+  );
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  setLoggerFromContext(context);
-  logTrace("profilePhoto: entry. Method: " + req.method);
+  const program = Effect.logTrace("profilePhoto: entry").pipe(
+    Effect.andThen(() =>
+      Effect.all([
+        getAuthenticatedUserId(req),
+        getBodyBuffer(req),
+        getContentType(req),
+      ])
+    ),
+    Effect.andThen(([userId, buffer, contentType]) =>
+      setProfilePicture(userId, contentType, buffer).pipe(
+        Effect.andThen(getProfileWithFormsByProfile(userId)),
+        Effect.andThen((data) => ({ data })),
+        Effect.andThen(S.encode(SetProfilePhotoResponse)),
+        Effect.andThen((body) =>
+          Effect.succeed({
+            status: 200,
+            body,
+          })
+        ),
+        Effect.catchTags({
+          ParseError: (e) =>
+            Effect.logError(
+              `ParseError when setting profile photo for user ${userId}`,
+              e
+            ).pipe(Effect.andThen(Effect.succeed({ status: 500 }))),
+        })
+      )
+    ),
+    Effect.catchTags({
+      ProfileNotFound: () => Effect.succeed({ status: 404 }),
+      NoBodyBuffer: () =>
+        Effect.succeed({ status: 400, body: "No profile image uploaded" }),
+      NoContentTypeHeader: () =>
+        Effect.succeed({
+          status: 400,
+          body: "No content type included in request",
+        }),
+      UserNotAuthenticated: () => Effect.succeed({ status: 401 }),
+    })
+  );
 
-  if (req.method !== "POST") {
-    logTrace(`profilePhoto: Invalid HTTP method: ${req.method}`);
-    context.res = {
-      status: 405,
-      headers: {
-        Allow: "POST",
-      },
-    };
-    return;
-  }
+  const logLayer = createLoggerLayer(context);
 
-  const userInfo = getUserInfo(req);
-  if (userInfo) {
-    logTrace(
-      `profilePhoto: User is authenticated. User ID: ${userInfo.userId}/${userInfo.identityProvider}`
-    );
-
-    context.res = await handlePostProfilePhoto(req, userInfo);
-  } else {
-    logTrace(`profilePhoto: User is not authenticated.`);
-    context.res = {
-      status: 401,
-      body: "Cannot alter profile photo when not authenticated.",
-    };
-  }
+  context.res = await Effect.runPromise(
+    program.pipe(
+      Effect.provide(repositoriesLayerLive),
+      Logger.withMinimumLogLevel(LogLevel.Trace),
+      Effect.provide(logLayer)
+    )
+  );
 };
 
 export default httpTrigger;
