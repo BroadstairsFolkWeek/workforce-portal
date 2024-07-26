1d47cbf26643b71773feb4d47bd897cfce91ee3a
Daniel Watford <daniel@watfordconsulting.com>
2024-07-26 08:16:35 +0000

    Remove unused typings around user logins and their persistance to SharePoint lists

--- api/model/interfaces/user-login.ts
+++ api/model/interfaces/user-login.ts
@@ -1,5 +1,4 @@
 import { Schema as S } from "@effect/schema";
-import { ModelProfileId } from "./profile";
 
 export const ModelCoreUserLogin = S.Struct({
   displayName: S.propertySignature(S.String).pipe(S.fromKey("Title")),
@@ -12,36 +11,3 @@ export const ModelCoreUserLogin = S.Struct({
 });
 export interface ModelCoreUserLogin
   extends S.Schema.Type<typeof ModelCoreUserLogin> {}
-
-const ModelUserLoginMetadata = S.Struct({
-  profileId: S.propertySignature(ModelProfileId).pipe(S.fromKey("ProfileId")),
-});
-
-const ModelUserLoginPersistanceData = S.Struct({
-  dbId: S.propertySignature(S.NumberFromString).pipe(S.fromKey("id")),
-  createdDate: S.propertySignature(S.DateFromString).pipe(S.fromKey("Created")),
-  modifiedDate: S.propertySignature(S.DateFromString).pipe(
-    S.fromKey("Modified")
-  ),
-});
-
-export const ModelAddableUserLogin = S.extend(
-  ModelCoreUserLogin,
-  ModelUserLoginMetadata
-);
-
-export const ModelPersistedUserLogin = S.extend(
-  ModelCoreUserLogin,
-  S.extend(ModelUserLoginMetadata, ModelUserLoginPersistanceData)
-);
-
-export interface ModelPersistedUserLogin
-  extends S.Schema.Type<typeof ModelPersistedUserLogin> {}
-
-export interface ModelAddableUserLogin
-  extends S.Schema.Type<typeof ModelAddableUserLogin> {}
-
-export interface ModelEncodedPersistedUserLogin
-  extends S.Schema.Encoded<typeof ModelPersistedUserLogin> {}
-export interface ModelEncodedAddableUserLogin
-  extends S.Schema.Encoded<typeof ModelAddableUserLogin> {}
